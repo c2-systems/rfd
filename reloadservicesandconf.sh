4a22b9d64092bcc547af6ae309af7ca44cea5161
jsteffensen <jsteffensen@users.noreply.github.com>
2025-09-06 18:11:25 +0200

    Update reloadservicesandconf.sh

--- reloadservicesandconf.sh
+++ reloadservicesandconf.sh
@@ -2,6 +2,7 @@
 # Download configuration files
 echo "Downloading configuration files..."
 sudo curl -o /home/toor/kismet.conf https://raw.githubusercontent.com/c2-systems/rfd/refs/heads/main/kismet.conf
+sudo curl -o /usr/local/etc/kismet_logging.conf https://raw.githubusercontent.com/c2-systems/rfd/refs/heads/main/kismet_logging.conf
 sudo curl -o /etc/systemd/system/wlan1-monitor.service https://raw.githubusercontent.com/c2-systems/rfd/refs/heads/main/wlan1-monitor.service
 sudo curl -o /etc/systemd/system/kismet-boot.service https://raw.githubusercontent.com/c2-systems/rfd/refs/heads/main/kismet-boot.service
 sudo curl -o /etc/systemd/system/kismet-uploader.service https://raw.githubusercontent.com/c2-systems/rfd/refs/heads/main/kismet-uploader.service
