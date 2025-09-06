3a68e542e54d0f256deb0eeefa0203267bd64b55
jsteffensen <jsteffensen@users.noreply.github.com>
2025-09-06 11:17:24 +0200

    Update filemonitor.sh

--- filemonitor.sh
+++ filemonitor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 while true; do
-   echo "$(date '+%H:%M:%S'):"
    for file in /home/toor/*.kismet; do
        if [ -f "$file" ]; then
-           echo "  $(basename "$file"): $(du -h "$file" | cut -f1)"
+           echo "$(date '+%H:%M:%S'):  $(basename "$file"): $(du -h "$file" | cut -f1)"
        fi
    done
    echo ""
