6ba9ae162a179f04a54fa1c40228ee50f2e0c7ce
jsteffensen <jsteffensen@users.noreply.github.com>
2025-11-06 17:44:44 +0100

    Update filemonitor.sh

--- filemonitor.sh
+++ filemonitor.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-while true; do
-   for file in /home/toor/*.kismet*; do
-       if [ -f "$file" ]; then
-           echo "$(date '+%H:%M:%S'):  $(basename "$file"): $(du -h "$file" | cut -f1)"
-       fi
-   done
-   echo ""
-   sleep 60
-done
+sudo journalctl -u kismet-uploader.service -f
